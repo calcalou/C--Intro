d4bf76cb9b11dafd690064255612c0a9babfc58b
Nicolas <n.bouniqmercier@gmail.com>
2024-10-03 12:00:55 +0200

    commit_2
    Ajout des tests unitaire dans le projet.
    Test de toutes les méthodes du projet.

---
+++ TestProject/CercleTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    [TestClass]
+    public class TestsCercle
+    {
+        [TestMethod]
+        public void TestCerclePerimetre()
+        {
+            Point A = new Point(2, 2);
+            Cercle cercle = new Cercle(A, 10);
+
+            double perimetre = cercle.Perimetre();
+
+            Assert.AreEqual(62.8318530717959, perimetre, 0.0001, "Le périmètre calculé est incorrect.");
+        }
+
+        [TestMethod]
+        public void TestCercleAire()
+        {
+            Point A = new Point(2, 2);
+            Cercle cercle = new Cercle(A, 10);
+
+            double aire = cercle.Aire();
+
+            Assert.AreEqual(314.159265358979, aire, 0.0001, "L'aire calculé est incorrecte.");
+        }
+
+        [TestMethod]
+        public void TestCercleToString()
+        {
+            Point A = new Point(2, 2);
+            Cercle cercle = new Cercle(A, 10);
+
+            string result = cercle.ToString();
+
+            Assert.AreEqual("Centre : (2,2), Rayon : 10", result, "La méthode ToString() du cercle ne retourne pas le format correct.");
+        }
+    }
+}
